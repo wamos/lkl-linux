7fec71b68189bba9c21cd58b507e436c21ccd165
Christian Priebe <cp3213@ic.ac.uk>
2019-06-03 07:58:30 +0000

    Increase reserved percpu memory to prevent new percpu allocation at runtime which requires an MMU architecture when SMP is enabled.
    The percpu allocation fails during ext4 initialisation due to a call to
    pcpu_get_vm_areas returning NULL with !CONFIG_MMU.

--- include/linux/percpu.h
+++ include/linux/percpu.h
@@ -63,7 +63,7 @@
  * intelligent way to determine this would be nice.
  */
 #if BITS_PER_LONG > 32
-#define PERCPU_DYNAMIC_RESERVE		(28 << 10)
+#define PERCPU_DYNAMIC_RESERVE		(29 << 10)
 #else
 #define PERCPU_DYNAMIC_RESERVE		(20 << 10)
 #endif
