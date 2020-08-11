9a975bf4e5f25dbb63ed2f14611a69508048ec27
Jörg Thalheim <joerg@thalheim.io>
2020-08-11 14:03:33 +0000

    spdk: also define ioctl number for debug hook

--- include/uapi/linux/spdk.h
+++ include/uapi/linux/spdk.h
@@ -6,6 +6,7 @@
 #define SPDK_CTL_ADD 0x4C80
 #define SPDK_REQ_COMPLETE 0x4C81
 #define SPDK_CTL_SHUTDOWN 0x4C82
+#define SPDK_CTL_DEBUG 0x4C83
 
 // Same definition as spdk_ctrlr_entry in spdk_context.h.
 // Copied here to break dependency cycles.
